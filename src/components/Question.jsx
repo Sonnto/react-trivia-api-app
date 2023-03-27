018450a17e5b5ab3a1b11696c5843f32feeaf968
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-03-27 13:00:33 -0400

    init

---
+++ src/components/Question.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+export default function Question() {
+  const [category, setCategory] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [revealed, setRevealed] = useState(false);
+
+  useEffect(() => {
+    const getQuestion = async () => {
+      let response = await fetch(
+        "https://opentdb.com/api.php?amount=1&type=boolean"
+      );
+      let data = await response.json();
+      console.log(`Here it is: ${data}`);
+
+      setCategory(data.results[0].category);
+      setQuestion(data.results[0].question);
+      setAnswer(data.results[0].correct_answer);
+    }
+    getQuestion();
+  }, []);
+
+  function revealButton() {
+    setRevealed(true);
+  }
+
+  let theAnswer = <button onClick={revealButton}>Reveal Answer</button>;
+
+  if (revealed) {
+    theAnswer = <p>{answer}</p>;
+  }
+
+  return (
+    <div>
+      <h2>{category}</h2>
+      <p>{question}</p>
+      {theAnswer}
+    </div>
+  );
+}
---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+import Question from "../components/Question";
+
+export default function Home() {
+  return (
+    <main id="main">
+      <h1>Today's Trivia Question</h1>
+      <Question />
+    </main>
+  );
+}
